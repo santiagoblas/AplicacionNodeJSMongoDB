13d2914084e8a04a7a7f62b22f058c81bae22d08
santiagoblas <santiagob.beitia@gmail.com>
2018-12-04 12:16:46 -0300

    Signup

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const User = require('../models/User')
 
 router.get('/users/signin', (req,res) => {
     res.render('users/signin');
@@ -6,6 +7,47 @@ router.get('/users/signin', (req,res) => {
 
 router.get('/users/signup', (req,res) => {
     res.render('users/signup');
+});
+
+router.post('/users/signup', async (req,res) => {
+    const {
+        name, 
+        email, 
+        password, 
+        confirm_password
+    } = req.body;
+
+    const errors = []
+
+    if(name.length <= 0) {
+        errors.push({text: "El Nombre es requerido"});
+    }
+    if(email.length <= 0) {
+        errors.push({text: "El E-mail es requerido"});
+    }
+    if(password != confirm_password) {
+        errors.push({text: "Las contraseñas no coinciden"});
+    }
+    if(password.length < 8) {
+        errors.push({text: "La Contraseña debe tener al menos 8 caracteres"});
+    }
+    if(errors.length > 0) {
+        res.render('users/signup', {errors, name, email, password, confirm_password});
+    } else {
+        const emailUser = await User.findOne({email: email});
+
+        if(emailUser) {
+            req.flash("error_msg", "El E-mail ya fue utilizado");
+            res.redirect("/users/signup");
+        }
+
+        const user = new User({name, email, password});
+        user.password = await user.encryptPassword(password);
+        await user.save();
+
+        req.flash("success_msg", "Usuario registrado");
+        res.redirect("/users/signin");
+    }
 });
 
 module.exports = router;
